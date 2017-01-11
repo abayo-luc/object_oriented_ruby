5d3d1c6a80acfbad3498471fbbebc9313fc34504
Jean Luc Abayo <luc.bayo@gmail.com>
2017-01-11 14:53:51 +0200

    added javascript syntax

--- store_item.rb
+++ store_item.rb
@@ -1,6 +1,10 @@
 item_1 = {:names =>"HP Pro-book", :color =>"Black", :Price =>"$150"}
 item_2 = {:names =>"HTC M7", :color =>"Black and white", :price =>"$120"}
-itme_3 = {:names =>"Ipad", :color =>"rose", :price =>"$220"}
+item_3 = {:names =>"Ipad", :color =>"rose", :price =>"$220"}
 puts item_1[:names]
 
 #jave script syntax
+product_1 = {names: 'HP Pro-book', color: 'Black', price: '$150'}
+product_2 = {names: 'HTC M7', color: 'Black and white', price: '$120'}
+product_3 = {names: 'Ipad', color: 'rose', price: '$220'}
+puts "the name of product is #{product_3[:names]} with #{product_3[:color]}, and it cost #{product_3[:price]}"

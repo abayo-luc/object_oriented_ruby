a517ef15bc0d931b81d59ff479ddc9a27057638e
Jean Luc Abayo <luc.bayo@gmail.com>
2017-01-11 14:18:52 +0200

    inital commit

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+item_1 = {:name =>"HP Pro-book", :color =>"Black", :Price =>"$150"}
+item_2 = {:name =>"HTC M7", :color =>"Black and white", :price =>"$120"}
+itmes_3 = {:name =>"Ipad", :color =>"rose", :price =>"$220"}

305fa0f68436a7a73a957d8313502b9137f7815c
Jean Luc Abayo <luc.bayo@gmail.com>
2017-01-11 14:22:54 +0200

    second changes

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,4 @@
 item_1 = {:name =>"HP Pro-book", :color =>"Black", :Price =>"$150"}
 item_2 = {:name =>"HTC M7", :color =>"Black and white", :price =>"$120"}
-itmes_3 = {:name =>"Ipad", :color =>"rose", :price =>"$220"}
+itme_3 = {:name =>"Ipad", :color =>"rose", :price =>"$220"}
+p item_1[name]

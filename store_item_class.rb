d7d8ebb6f84b415f188ebdd19e2e67579412794d
Jean Luc Abayo <luc.bayo@gmail.com>
2017-01-12 11:12:37 +0200

    class refactored

--- store_item_class.rb
+++ store_item_class.rb
@@ -1,22 +1,15 @@
 class Store
+
+  attr_writer :product_names, :product_color, :product_names
+  attr_reader :price
+
   def initialize(product_names,product_color,product_price)
     @name = product_names
     @color = product_color
     @price = product_price
 
   end
-  def product_names
-    @name
-  end
-  def product_color
-    @color
-  end
-  def product_price
-    @price
-  end
-  def price(product_price)
-    @price =product_price
-  end
+  
   def info
     "#{@product_names} with #{@product_color} color cost #{@product_price}"
   end
@@ -26,3 +19,4 @@ product2 = Store.new("HTC M7", "black & white", 160)
 product3 = Store.new("Ipad", "rose", 280)
 p product1.price(100)
 puts product3.product_color
+p product3.price(300)
